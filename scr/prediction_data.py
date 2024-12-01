8a07771958787d14aba0f9cbdac8431ea3eb4419
Tadek <tadek.waluga@gmail.com>
2024-12-01 20:54:45 +0100

    updated validation frame recognition

--- scr/prediction_data.py
+++ scr/prediction_data.py
@@ -2,33 +2,54 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import plotly.express as px
+import cv2
+import pytesseract as pt
 
 
 # Load model
 model = tf.keras.models.load_model('./models/license_plate_frame_recognition_model.keras')
 print('Model loaded Sucessfully')
 
+# Create pipeline
 path = 'data/TEST/TEST.jpeg'
-image = load_img(path) # PIL object
-image = np.array(image,dtype=np.uint8) # 8 bit array (0,255)
-image1 = load_img(path,target_size=(224,224))
-image_arr_224 = img_to_array(image1)/255.0  # Convert into array and get the normalized output
+def object_detection(path):
+    
+    # Read image
+    image = load_img(path) # PIL object
+    image = np.array(image,dtype=np.uint8) # 8 bit array (0,255)
+    image1 = load_img(path,target_size=(224,224))
+    
+    # Data preprocessing
+    image_arr_224 = img_to_array(image1)/255.0 # Convert to array & normalized
+    h,w,d = image.shape
+    test_arr = image_arr_224.reshape(1,224,224,3)
+    
+    # Make predictions
+    coords = model.predict(test_arr)
+    print('Normalized coordinates: ', coords)
+    
+    # Denormalize the values
+    denorm = np.array([w,w,h,h])
+    coords = coords * denorm
+    coords = coords.astype(np.int32)
+    print('Denormalized coordinates:', coords)
+    
+    # Draw bounding on top the image
+    xmin, xmax,ymin,ymax = coords[0]
+    pt1 = (xmin, ymin)
+    pt2 = (xmax, ymax)
+    print(pt1, pt2)
+    cv2.rectangle(image,pt1,pt2,(0,255,0),3)
+    return image, coords
 
-# Size of the orginal image
-h,w,d = image.shape
-print('Height of the image =',h)
-print('Width of the image =',w)
+image, cods = object_detection(path)
 
-# fig = px.imshow(image)
-# fig.update_layout(width=700, height=500,  margin=dict(l=10, r=10, b=10, t=10), xaxis_title='Figure 13 - TEST Image')
+img = np.array(load_img(path))
+xmin, xmax, ymin, ymax = cods[0]
+roi = img[ymin:ymax, xmin:xmax]
 
-test_arr = image_arr_224.reshape(1,224,224,3)
-
-coords = model.predict(test_arr)
-
-print(coords)
-
-denorm = np.array([w,w,h,h])
-coords = coords * denorm
-coords = coords.astype(np.int32)
-print('Denormalized coordinates:', coords)
+# extract text from image
+# https://github.com/tesseract-ocr/tessdoc?tab=readme-ov-file#tesseract-user-manual
+pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+text = pt.image_to_string(roi)
+print(text)
